404b02843665cc10bfc3b96d9c74052c835d9e95
Dmitry Maslov <dmaslov81@gmail.com>
2021-10-08 23:02:12 +1000

    Working on blog section (website)

---
+++ www/blog/nav.js
@@ -0,0 +1,75 @@
+﻿var currentYear = 0;
+var currentMonth = 0;
+var currentLanguage = "en";
+
+function toggleYear(year, lang)
+{
+	if (lang == "en" || lang == "ru")
+		currentLanguage = lang;
+
+	for (let i = 2019; i <= 2021; ++i)
+	{
+		let yearTag = document.getElementById("nav-y-" + i);
+
+		//yearTag.onclick = function() { toggleYear(i); };
+		yearTag.title = i + ((currentLanguage == "ru") ? " год" : " year");
+
+		yearTag.style.backgroundColor = null;
+		yearTag.style.color = null;
+		yearTag.style.cursor = null;
+	}
+
+	if (year >= 2019 && year <= 2021)
+	{
+		currentYear = year;
+		let yearTag = document.getElementById("nav-y-" + year);
+
+		yearTag.style.backgroundColor = "#fc6";
+		yearTag.style.color = "inherit";
+		yearTag.style.cursor = "pointer";
+	}
+}
+
+function toggleMonth(month, lang)
+{
+	if (lang == "en" || lang == "ru")
+		currentLanguage = lang;
+
+	let monthNamesEn = ["", "January", "February", "March", "April", "May", "June",
+		"July", "August", "September", "October", "November", "December"];
+	let monthNamesRu = ["", "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+		"Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+	let monthNames = (currentLanguage == "ru") ?
+		monthNamesRu : monthNamesEn;
+
+	for (let i = 1; i <= 12; ++i)
+	{
+		let monthTag = document.getElementById("nav-m-" + i);
+
+		//monthTag.onclick = function() { toggleMonth(i); };
+		monthTag.textContent = (i < 10) ? "0" + i : i;
+		monthTag.title = monthNames[i];
+
+		monthTag.style.backgroundColor = null;
+		monthTag.style.color = null;
+		monthTag.style.cursor = null;
+	}
+
+	if (month >= 1 && month <= 12)
+	{
+		currentMonth = month;
+		let monthTag = document.getElementById("nav-m-" + month);
+
+		monthTag.textContent = monthNames[month];
+
+		monthTag.style.backgroundColor = "#fc6";
+		monthTag.style.color = "inherit";
+		monthTag.style.cursor = "pointer";
+	}
+}
+
+function initNavigation(lang)
+{
+	toggleYear(2021, lang);
+	toggleMonth(9, lang);
+}
