e64cdcc7ed284cc9de6b10e5e17f8d83872b4837
Dmitry Maslov <dmaslov81@gmail.com>
2021-10-12 23:39:20 +1000

    Refactored CSS, fixed codestyle, and some minor improvements (website)

--- www/blognav.js
+++ www/blognav.js
@@ -4,13 +4,11 @@ var currentYear = 0;
 var currentMonth = 0;
 var currentLanguage = "en";
 
-function toggleYear(year, lang)
-{
-	if (lang == "en" || lang == "ru")
+function toggleYear(year, lang) {
+	if (lang === "en" || lang === "ru")
 		currentLanguage = lang;
 
-	for (let i = 2019; i <= 2021; ++i)
-	{
+	for (let i = 2019; i <= 2021; ++i) {
 		let yearTag = document.getElementById("nav-y-" + i);
 
 		//yearTag.onclick = function() { toggleYear(i); };
@@ -21,8 +19,7 @@ function toggleYear(year, lang)
 		yearTag.style.cursor = null;
 	}
 
-	if (year >= 2019 && year <= 2021)
-	{
+	if (year >= 2019 && year <= 2021) {
 		currentYear = year;
 		let yearTag = document.getElementById("nav-y-" + year);
 
@@ -32,20 +29,18 @@ function toggleYear(year, lang)
 	}
 }
 
-function toggleMonth(month, lang)
-{
-	if (lang == "en" || lang == "ru")
+function toggleMonth(month, lang) {
+	if (lang === "en" || lang === "ru")
 		currentLanguage = lang;
 
 	let monthNamesEn = ["", "January", "February", "March", "April", "May", "June",
 		"July", "August", "September", "October", "November", "December"];
 	let monthNamesRu = ["", "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
 		"Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
-	let monthNames = (currentLanguage == "ru") ?
+	let monthNames = (currentLanguage === "ru") ?
 		monthNamesRu : monthNamesEn;
 
-	for (let i = 1; i <= 12; ++i)
-	{
+	for (let i = 1; i <= 12; ++i) {
 		let monthTag = document.getElementById("nav-m-" + i);
 
 		//monthTag.onclick = function() { toggleMonth(i); };
@@ -57,8 +52,7 @@ function toggleMonth(month, lang)
 		monthTag.style.cursor = null;
 	}
 
-	if (month >= 1 && month <= 12)
-	{
+	if (month >= 1 && month <= 12) {
 		currentMonth = month;
 		let monthTag = document.getElementById("nav-m-" + month);
 
@@ -70,38 +64,32 @@ function toggleMonth(month, lang)
 	}
 }
 
-function initNavigation(lang)
-{
+function initNavigation(lang) {
 	toggleYear(2021, lang);
 	toggleMonth(9, lang);
 }
 
 /* Post pages */
 
-function initPostLink(elementId, postLink)
-{
+function initPostLink(elementId, url) {
 	let navElement = document.getElementById(elementId);
 
 	navElement.classList.add("postlink");
 	navElement.onclick = function() {
-		window.location.href = postLink;
+		window.location.href = url;
 	};
 }
 
-function initPrevPostLinks(prev)
-{
-	if (prev && prev != "")
-	{
-		initPostLink("link-prev-top", prev);
-		initPostLink("link-prev-bottom", prev);
+function initPrevPostLinks(url) {
+	if (url && url != "") {
+		initPostLink("link-prev-top", url);
+		initPostLink("link-prev-bottom", url);
 	}
 }
 
-function initNextPostLinks(next)
-{
-	if (next && next != "")
-	{
-		initPostLink("link-next-top", next);
-		initPostLink("link-next-bottom", next);
+function initNextPostLinks(url) {
+	if (url && url != "") {
+		initPostLink("link-next-top", url);
+		initPostLink("link-next-bottom", url);
 	}
 }
