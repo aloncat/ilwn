57b0b44e626d3eea668503c881e3ae0d3c1ce5f2
Dmitry Maslov <dmaslov81@gmail.com>
2021-10-16 18:59:14 +1000

    AWS Lambda@Edge functions (website)

---
+++ script/aws-lambda/dirRedirect.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+exports.handler = (event, context, callback) => {
+	let request = event.Records[0].cf.request;
+
+	if (request.uri === "/ru" ||
+		request.uri === "/blog" || request.uri === "/ru/blog" ||
+		request.uri === "/alsn" || request.uri === "/ru/alsn" ||
+		request.uri === "/mdpn" || request.uri === "/ru/mdpn" ||
+		request.uri === "/p196" || request.uri === "/ru/p196")
+	{
+		request.uri += "/";
+	}
+
+	// Replace any '/' at the end of a URI with default index
+	request.uri = request.uri.replace(/\/$/, "/index.html");
+
+	return callback(null, request);
+};
---
+++ script/aws-lambda/securityHeaders.js
@@ -0,0 +1,37 @@
+﻿'use strict';
+
+exports.handler = (event, context, callback) => {
+	let response = event.Records[0].cf.response;
+	let headers = response.headers;
+
+	headers["strict-transport-security"] = [{
+		key: "Strict-Transport-Security",
+		value: "max-age=63072000"
+	}];
+	headers["content-security-policy"] = [{
+		key: "Content-Security-Policy",
+		value: "default-src 'none'; " +
+			"img-src 'self'; object-src 'none'; " +
+			"script-src 'self' 'unsafe-inline'; " +
+			"style-src 'self' 'unsafe-inline'"
+	}];
+	headers["referrer-policy"] = [{
+		key: "Referrer-Policy",
+		value: "no-referrer, strict-origin-when-cross-origin"
+	}];
+
+	headers["x-content-type-options"] = [{
+		key: "X-Content-Type-Options",
+		value: "nosniff"
+	}];
+	headers["x-frame-options"] = [{
+		key: "X-Frame-Options",
+		value: "DENY"
+	}];
+	headers["x-xss-protection"] = [{
+		key: "X-XSS-Protection",
+		value: "1; mode=block"
+	}];
+
+	return callback(null, response);
+};
