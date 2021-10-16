331299f72ba76c7995787238c617547e36412233
Dmitry Maslov <dmaslov81@gmail.com>
2021-10-16 15:50:15 +1000

    Updated AWS CLI scripts to fix text/plain MIME type for .js files

---
+++ script/fix-registry-js-class.bat
@@ -0,0 +1,13 @@
+@echo off
+
+net session
+if %ERRORLEVEL% NEQ 0 goto elevate
+
+cls
+reg add HKLM\Software\Classes\.js /v "Content Type" /t REG_SZ /d "text/javascript" /f
+pause & exit /b
+
+:elevate
+cd /d "%~dp0"
+mshta "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1); close();"
+exit /b
