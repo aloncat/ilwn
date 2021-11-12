883ad4fce568d4f38bc5bac5566f0246e3bb7e86
Dmitry Maslov <dmaslov81@gmail.com>
2021-11-12 10:39:48 +1000

    Website update

--- www/js/checkpal.js
+++ www/js/checkpal.js
@@ -12,7 +12,7 @@ const FX_STAY_TIMEOUT = 200; // Milliseconds
 const FX_FADE_TIMEOUT = "1.0s"; // Duration (CSS)
 const ONCOPY_FX_COLOR = "#0c0"; // Color (CSS)
 const URL_PREFIX = "https://dmaslov.me";
-const SCRIPT_VERSION = "2021.11.03";
+const SCRIPT_VERSION = "2021.11.12";
 
 (function () {
 	loadingText.style.display = "none";
@@ -1000,8 +1000,8 @@ function getKnownNumbers() {
 	p[264] = pal("200000000000000998850438138599", false);
 	p[265] = pal("103420000000000994470264099994", false);
 	p[266] = pal("50002000000000254084565399849", false);
-	p[267] = pal("10000000000000876587282257696", false);
-	p[268] = pal("7000008000000030194299423429", false);
+	p[267] = pal("10000000000000540430982257696", false);
+	p[268] = pal("7000000000000032256094025499", false);
 	p[269] = pal("1000000020000060596998216217", false);
 	p[270] = pal("1000000000050059982414991789", false);
 	p[271] = pal("1000000000000394741252995844", false);
