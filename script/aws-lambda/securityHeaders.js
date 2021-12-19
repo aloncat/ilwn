5c59af17640881e25130a63ecb7819db09ae0039
Dmitry Maslov <dmaslov81@gmail.com>
2021-12-19 19:48:57 +1000

    Enabled Google Analytics for website

--- script/aws-lambda/securityHeaders.js
+++ script/aws-lambda/securityHeaders.js
@@ -13,8 +13,10 @@ exports.handler = (event, context, callback) => {
 	if (requestUri.slice(-5) === ".html") {
 		headers["content-security-policy"] = [{
 			key: "Content-Security-Policy",
-			value: "default-src 'none'; img-src 'self'" +
-				"; script-src 'self' 'unsafe-inline'" +
+			value: "default-src 'none'" +
+				"; img-src 'self' www.googletagmanager.com www.google-analytics.com" +
+				"; script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://ssl.google-analytics.com 'unsafe-inline'" +
+				"; connect-src https://www.google-analytics.com" +
 				"; style-src 'self' 'unsafe-inline'" +
 				"; frame-src https:"
 		}];
