b620e41cf6a579d02b11bf0eab8e0f929421d158
Dmitry Maslov <dmaslov81@gmail.com>
2022-02-18 12:00:49 +1000

    Refactoring for p.m.n support

--- source/eslp/main.cpp
+++ source/eslp/main.cpp
@@ -4,7 +4,7 @@
 
 #include "pch.h"
 
-#include "diophantine.h"
+#include "factorsearch.h"
 
 #include <auxlib/print.h>
 #include <core/util.h>
@@ -14,36 +14,37 @@ int wmain(int argCount, const wchar_t* args[], const wchar_t* envVars[])
 {
 	util::CheckMinimalRequirements();
 
-	aux::Printc(L"Equal Sums of Like Powers (ESLP)/iLWN. Standalone app. Ver. 0.21a\n"
+	aux::Printc(L"Equal Sums of Like Powers (ESLP)/iLWN. Standalone app. Ver. 0.30a\n"
 		"#8For more information, please visit us at #9https://dmaslov.me/eslp/\n");
 
-	// Программа принимает 2 обязательных и 1 опциональный аргумент командной строки: 1) степень уравнения,
-	// 2) количество слагаемых в его правой части, 3) опциональное значение самого старшего коэффициента
-	// (в левой части), с которого будет начат поиск (при его отсутствии будет использовано значение 2)
+	// Программа принимает 3 обязательных и 1 опциональный аргумент командной строки: 1) степень
+	// уравнения, 2) количество слагаемых в его левой части, 3) количество слагаемых в его правой
+	// части, 4) опциональное значение самого старшего коэффициента левой части, с которого будет
+	// начат поиск (при его отсутствии будет использовано значение 2)
 
 	if (argCount <= 1)
 	{
 		// Справка для пустой командной строки
-		aux::Printc("#15Usage: #6eslp.exe#7 power count [factor]\n");
+		aux::Printc("#15Usage: #6eslp.exe#7 power left right [factor]\n");
 		return 0;
 	}
 
-	if (argCount == 3 || argCount == 4)
+	if (argCount >= 4)
 	{
 		int power = wcstol(args[1], nullptr, 10);
-		int count = wcstol(args[2], nullptr, 10);
-		int hiFactor = (argCount >= 4) ? wcstol(args[3], nullptr, 10) : 2;
+		int leftCount = wcstol(args[2], nullptr, 10);
+		int rightCount = wcstol(args[3], nullptr, 10);
 
-		if (power >= 1 && power <= FactorSearch::MAX_POWER && count >= 2 &&
-			count <= FactorSearch::MAX_FACTOR_COUNT && hiFactor >= 0)
+		constexpr auto maxCount = FactorSearch::MAX_FACTOR_COUNT;
+		if (power >= 1 && power <= FactorSearch::MAX_POWER && leftCount >= 1 && rightCount >= 2 &&
+			leftCount < maxCount && rightCount < maxCount && leftCount <= rightCount &&
+			leftCount + rightCount <= maxCount)
 		{
 			FactorSearch search;
-			// Ищем коэффициенты. При ошибке функция вернёт false
-			if (search.Run(power, count, hiFactor))
-				return 0;
-
-			// Что-то пошло не так. Сообщение об ошибке уже выведено
-			return 1;
+			// Ищем решения. Передаём в функцию только значение степени и количество коэффициентов
+			// уравнения (начальное значение старшего коэффициента функция получит самостоятельно).
+			// При возникновении ошибки функция вернёт false и выведет сообщение в окно консоли
+			return search.Run(power, leftCount, rightCount) ? 0 : 1;
 		}
 	}
 
