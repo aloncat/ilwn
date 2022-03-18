498a78e7e18a326f997044c35088f97193c86e63
Dmitry Maslov <dmaslov81@gmail.com>
2022-03-18 17:30:03 +1000

    Experimental algorithm for x.2.2 (WIP)

---
+++ source/eslp/search-x22i.cpp
@@ -0,0 +1,262 @@
+﻿//∙ESLP/iLWN
+// Copyright (C) 2022 Dmitry Maslov
+// For conditions of distribution and use, see readme.txt
+
+#include "pch.h"
+#include "search-x22i.h"
+
+#include "progressman.h"
+
+#include <auxlib/print.h>
+#include <core/console.h>
+#include <core/debug.h>
+#include <core/strformat.h>
+
+//--------------------------------------------------------------------------------------------------------------------------------
+SearchX22i::SearchX22i()
+	: m_TaskCS(1000)
+{
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+bool SearchX22i::IsSuitable(int power, int leftCount, int rightCount, bool allowAll)
+{
+	// Экспериментальный алгоритм подходит для любых степеней. Но для степеней <3 его
+	// использование не имеет смысла из-за высокой скорости основного алгоритма "X22"
+	return allowAll && power >= 3 && leftCount == 2 && rightCount == 2;
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+std::wstring SearchX22i::GetAdditionalInfo() const
+{
+	Assert(m_Info.leftCount == 2 && m_Info.rightCount == 2);
+	return util::Format(L"#12experimental #7algorithm for #6#X.2.2#8 (%u MiB)",
+		m_SuperHashes.GetSize() / 1024);
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+void SearchX22i::BeforeCompute(unsigned upperLimit)
+{
+	SetSearchFn(this);
+
+	m_DecomposeFn = nullptr;
+	if (m_Powers->IsType<uint64_t>())
+		m_DecomposeFn = GetDecomposeFn<uint64_t>();
+	else if (m_Powers->IsType<UInt128>())
+		m_DecomposeFn = GetDecomposeFn<UInt128>();
+	Assert(m_DecomposeFn);
+
+	InitHashTable(m_Hashes, upperLimit);
+	m_SuperHashes.Init();
+	m_Tail = m_Head = 0;
+
+	// Оптимизация для чётных степеней проверена для всех степеней до 20-й включительно. Скорее всего,
+	// она работает и для других степеней, но я это не проверил (в программе степень ограничена 20-й)
+	static_assert(MAX_POWER <= 20, "Need to re-check optimization for even powers for x.2.2");
+	m_IsEvenPower = !(m_Info.power & 1) && (m_Info.power <= 20);
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+void SearchX22i::AfterCompute()
+{
+	// После обработки блока задания и остановки рабочих потоков в буфере не должно оставаться необработанных пар,
+	// иначе мы можем пропустить решение. При завершении блока заданий или прерывании поиска рабочие потоки перед
+	// тем, как приостановиться, должны были закончить обработку оставшихся в буфере пар
+	Assert(m_Head == m_Tail && "Some pairs are still unprocessed");
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+unsigned SearchX22i::GetChunkSize(unsigned hiFactor)
+{
+	return (hiFactor > 56000) ? 7200 :
+		7200 + (56000 - hiFactor) / 8;
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+void SearchX22i::InitFirstTask(Task& task, const std::vector<unsigned>& startFactors)
+{
+	// Задание задаёт только старший коэффициент левой части.
+	// 2-й коэффициент левой части перебирается в SearchFactors
+	Assert(!startFactors.empty());
+
+	task.factorCount = 1;
+	task.factors[0] = startFactors[0];
+
+	const size_t len = 16;
+	aux::Printc("#8\rInitializing super hashes...");
+
+	if (m_Powers->IsType<uint64_t>())
+		InitSuperHashes<uint64_t>(task.factors[0]);
+	else if (m_Powers->IsType<UInt128>())
+		InitSuperHashes<UInt128>(task.factors[0]);
+
+	auto s = "#8" + std::string(len, '\b') + "...";
+	s.append(len - 3, ' ');
+	aux::Printc(s + "\r");
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+void SearchX22i::SelectNextTask(Task& task)
+{
+	++task.factors[0];
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+void SearchX22i::WorkerFunction(Worker* worker)
+{
+	int head = m_Head.load(std::memory_order_acquire);
+	int tail = m_Tail.load(std::memory_order_relaxed);
+
+	int count = tail - head;
+	count += (count < 0) ? MAX_PAIRS : 0;
+
+	if (count < MAX_PAIRS * 3 / 4 && m_TaskCS.TryEnter())
+	{
+		thread::Lock lock(m_TaskCS, false);
+
+		if (GetNextTask(worker))
+		{
+			const unsigned task = worker->task.factors[0];
+			(this->*m_SearchFn)(worker, m_Powers->GetData());
+			lock.Leave();
+
+			for (;;)
+			{
+				(this->*m_DecomposeFn)(worker, m_Powers->GetData());
+
+				head = m_Head.load(std::memory_order_acquire);
+				tail = m_Tail.load(std::memory_order_relaxed);
+				if (head == tail || m_Pairs[head].k0 > task)
+				{
+					OnTaskDone(worker);
+					return;
+				}
+			}
+		}
+
+		worker->shouldPause = true;
+	}
+
+	(this->*m_DecomposeFn)(worker, m_Powers->GetData());
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+template<class NumberT>
+void SearchX22i::InitSuperHashes(unsigned startFactor)
+{
+	auto& console = util::SystemConsole::Instance();
+	const NumberT* powers = static_cast<const NumberT*>(m_Powers->GetData());
+
+	unsigned k0;
+	const auto uv = powers[startFactor] + 1;
+	// Пропустим задания, для которых макс. сумма степеней двух коэф-в
+	// меньше мин. суммы степеней коэффициентов стартового задания (uv)
+	for (k0 = 2; k0 < startFactor && uv > (powers[k0] << 1); ++k0);
+
+	for (size_t it = 0; k0 < startFactor; ++k0)
+	{
+		const auto pk0 = powers[k0] & ~0llu;
+		const unsigned delta = (m_IsEvenPower && !(k0 & 1)) ? 2 : 1;
+
+		for (unsigned k1 = 1; k1 <= k0; k1 += delta)
+		{
+			auto v = pk0 + (powers[k1] & ~0llu);
+			m_SuperHashes.Insert(v);
+		}
+
+		if (!(++it & 63) && console.IsCtrlCPressed())
+			break;
+	}
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+template<class NumberT>
+AML_NOINLINE void SearchX22i::SearchFactors(Worker* worker, const NumberT* powers)
+{
+	// Массив коэффициентов
+	unsigned k[ProgressManager::MAX_COEFS];
+
+	// Старший коэф-т левой части
+	k[0] = worker->task.factors[0];
+
+	// Если значение k[0] чётно, то k[1] не может быть чётным при любой чётной степени, иначе решение будет
+	// непримитивным. Поэтому при чётных k[0] и степени мы будем проверять только нечётные значения k[1]
+	const unsigned delta = (m_IsEvenPower && !(k[0] & 1)) ? 2 : 1;
+
+	// Перебор 2-го коэф-та левой части
+	for (k[1] = 1; k[1] <= k[0]; k[1] += delta)
+	{
+		// Сумма в левой части уравнения
+		const auto z = powers[k[0]] + powers[k[1]];
+
+		if (!m_SuperHashes.Exists(z))
+		{
+			m_SuperHashes.Insert(z);
+			continue;
+		}
+
+		// TODO: добавление пары в буфер
+		//
+
+		// Вывод прогресса
+		if (!(++worker->progressCounter & 0xfff))
+		{
+			if (OnProgress(worker, k))
+				return;
+		}
+	}
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+template<class NumberT>
+AML_NOINLINE void SearchX22i::Decompose(Worker* worker, const NumberT* powers)
+{
+	// Массив коэффициентов
+	unsigned k[ProgressManager::MAX_COEFS];
+
+	// TODO: получение пары из буфера
+	//
+
+	// Сумма в левой части уравнения
+	const auto z = powers[k[0]] + powers[k[1]];
+
+	k[2] = 1;
+	// Пропускаем низкие значения старшего коэф-та
+	for (unsigned step = k[0] >> 1; step; step >>= 1)
+	{
+		auto f = k[2] + step;
+		if (z > powers[f - 1] * 2)
+			k[2] = f;
+	}
+
+	for (; k[2] < k[0]; ++k[2])
+	{
+		if (const auto lastFP = z - powers[k[2]]; m_Hashes.Exists(lastFP))
+		{
+			for (unsigned lo = 1, hi = k[2]; lo <= hi;)
+			{
+				unsigned mid = (lo + hi) >> 1;
+				if (auto v = powers[mid]; v < lastFP)
+					lo = mid + 1;
+				else if (v > lastFP)
+					hi = mid - 1;
+				else
+				{
+					k[3] = mid;
+					if (OnSolutionFound(worker, k))
+						return;
+					break;
+				}
+			}
+		}
+	}
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+template<class NumberT>
+FactorSearch::SearchFn SearchX22i::GetDecomposeFn()
+{
+	using Fn = void (SearchX22i::*)(Worker*, const NumberT*);
+	auto fn = static_cast<Fn>(&SearchX22i::template Decompose);
+	return reinterpret_cast<SearchFn>(fn);
+}
---
+++ source/eslp/search-x22i.h
@@ -0,0 +1,73 @@
+﻿//∙ESLP/iLWN
+// Copyright (C) 2022 Dmitry Maslov
+// For conditions of distribution and use, see readme.txt
+
+#pragma once
+
+#include "factorsearch.h"
+#include "hashtable.h"
+
+#include <core/threadsync.h>
+
+#include <atomic>
+#include <string>
+#include <vector>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//   SearchX22 - экспериментальный алгоритм для уравнений x.2.2
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//--------------------------------------------------------------------------------------------------------------------------------
+class SearchX22i : public FactorSearch
+{
+public:
+	SearchX22i();
+
+protected:
+	static bool IsSuitable(int power, int leftCount, int rightCount, bool allowAll);
+	virtual std::wstring GetAdditionalInfo() const override;
+
+	virtual void BeforeCompute(unsigned upperLimit) override;
+	virtual void AfterCompute() override;
+
+	virtual unsigned GetChunkSize(unsigned hiFactor) override;
+
+	virtual void InitFirstTask(Task& task, const std::vector<unsigned>& startFactors) override;
+	virtual void SelectNextTask(Task& task) override;
+
+	virtual void WorkerFunction(Worker* worker) override;
+
+	template<class NumberT>
+	void InitSuperHashes(unsigned startFactor);
+
+	template<class NumberT>
+	void SearchFactors(Worker* worker, const NumberT* powers);
+
+	template<class NumberT>
+	void Decompose(Worker* worker, const NumberT* powers);
+
+	template<class NumberT>
+	static SearchFn GetDecomposeFn();
+
+protected:
+	// Макс. количество пар в m_Pairs
+	static constexpr int MAX_PAIRS = 2048;
+
+	struct Pair {
+		unsigned k0;	// Старший коэффициент левой части
+		unsigned k1;	// Младший коэффициент левой части
+	};
+
+	HashTable<23> m_Hashes;				// Обычная хеш-таблица (для поиска коэффициента)
+	thread::CriticalSection m_TaskCS;	// Критическая секция для потока задания
+	SuperHashTable<35> m_SuperHashes;	// Большая хеш-таблица для значений пар
+	SearchFn m_DecomposeFn = nullptr;	// Указатель на функцию декомпозиции
+
+	Pair m_Pairs[MAX_PAIRS];			// Кольцевой буфер пар коэффициентов
+	std::atomic<int> m_Head;			// Индекс первого элемента (голова списка)
+	std::atomic<int> m_Tail;			// Индекс элемента, следующего за последним
+
+	bool m_IsEvenPower = false;			// true, если степень уравнения чётная
+};
