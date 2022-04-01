f605e2e14df999fd55c461c87d8130acd71e8601
Dmitry Maslov <dmaslov81@gmail.com>
2022-04-01 21:42:32 +1000

    Website: download. -> dl.

--- script/sync-download-bucket.bat
+++ script/sync-download-bucket.bat
@@ -1,7 +1,7 @@
 @echo off
 
-echo Syncing bucket 'download.dmaslov.me' with local copy...
+echo Syncing bucket 'dl.dmaslov.me' with local copy...
 
 set EP=--endpoint-url=https://storage.yandexcloud.net
-aws s3 sync ..\..\file\root s3://download.dmaslov.me --delete %EP%
+aws s3 sync ..\..\file\root s3://dl.dmaslov.me --delete %EP%
 pause
