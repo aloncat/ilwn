0a6f8cd090cacb09649bb91334110e918dceab79
Dmitry Maslov <dmaslov81@gmail.com>
2026-05-13 20:21:38 +1000

    Website update

--- www/js/pageheader.js
+++ www/js/pageheader.js
@@ -149,7 +149,7 @@ function insertFooter() {
 		  '<tr style="vertical-align: top">' +
 			'<td class="footer">' +
 			  '<div style="margin-right: 25px">' +
-				'Copyright &copy;&nbsp;2019-2022 Dmitry&nbsp;Maslov' +
+				'Copyright &copy;&nbsp;2019-2026 Dmitry&nbsp;Maslov' +
 			  '</div>' +
 			'</td>' +
 			'<td class="footer" style="text-align: right">' +
