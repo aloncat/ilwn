9a6097b39f7c40d9868bcfacdef9a57efc6446d3
Dmitry Maslov <dmaslov81@gmail.com>
2026-06-09 22:13:52 +1000

    WIP on refactoring

--- source/mdpn/aml/core/file.cpp
+++ source/mdpn/aml/core/file.cpp
@@ -6,67 +6,66 @@
 
 using namespace util;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//   BufferedFile
-//
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 BufferedFile::BufferedFile(size_t bufferSize)
-	: m_pBuffer(0)
+	: m_Buffer(nullptr)
 	, m_BufferSize(Clamp(bufferSize, 4u, 512u) * 1024)
 {
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 BufferedFile::~BufferedFile()
 {
 	Close();
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 void BufferedFile::Close()
 {
-	if (m_pBuffer)
+	if (m_Buffer)
 	{
 		FlushBuffer();
-		free(m_pBuffer);
-		m_pBuffer = 0;
+		free(m_Buffer);
+		m_Buffer = nullptr;
 	}
 	BinaryFile::Close();
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 bool BufferedFile::Flush()
 {
 	return FlushBuffer() && BinaryFile::Flush();
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 bool BufferedFile::FlushBuffer()
 {
-	if (!m_pBuffer) return false;
+	if (!m_Buffer)
+		return false;
+
 	// Позиция m_BufferPos, отличная от 0, может означать, что в буфере есть записанные данные.
-	// Но если при этом m_CachedSize не равен 0, то значит буфер находится в режиме чтения.
+	// Но если при этом m_CachedSize не равен 0, то значит буфер находится в режиме чтения
 	return !m_BufferPos || m_CachedSize || WriteCache();
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 long long BufferedFile::GetPosition() const
 {
-	if (!m_pBuffer) return -1;
+	if (!m_Buffer)
+		return -1;
 	// Если в буфере есть прочитанные данные, то независимо от реальной позиции файла
-	// (не важно, чему равен m_SeekError), возвращаем текущую позицию чтения в буфере.
-	if (m_CachedSize) return m_FilePos - m_CachedSize + m_BufferPos;
+	// (не важно, чему равен m_SeekError), возвращаем текущую позицию чтения в буфере
+	if (m_CachedSize)
+		return m_FilePos - m_CachedSize + m_BufferPos;
 	// Если не было ошибки позиционирования (т.е. m_FilePos совпадает с реальной позицией файла) или
 	// в буфере есть записанные данные, то возвращаем известную нам позицию. В случае наличия ошибки
 	// позиционирования и данных в буфере записи следующей операцией с файлом станет сохранение
-	// данных из буфера, при котором известная позиция станет реальной позицией файла.
-	if (!m_SeekError || m_BufferPos) return m_FilePos + m_BufferPos;
+	// данных из буфера, при котором известная позиция станет реальной позицией файла
+	if (!m_SeekError || m_BufferPos)
+		return m_FilePos + m_BufferPos;
 
 	// Если же была ошибка и данных в буфере нет, попытаемся выяснить реальную
-	// текущую позицию файла. В случае успеха запомним ее и сбросим ошибку.
+	// текущую позицию файла. В случае успеха запомним ее и сбросим ошибку
 	long long pos = BinaryFile::GetPosition();
 	if (pos >= 0)
 	{
@@ -76,21 +75,22 @@ long long BufferedFile::GetPosition() const
 	return pos;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 long long BufferedFile::GetSize() const
 {
 	long long size = BinaryFile::GetSize();
 	// Если в буфере есть записанные данные, то они могут увеличить размер файла,
-	// если находятся за концом имеющихся на текущий момент данных в файле.
+	// если находятся за концом имеющихся на текущий момент данных в файле
 	if (m_BufferPos && !m_CachedSize && (size >= 0))
 	{
 		long long position = m_FilePos + m_BufferPos;
-		if (position >= size) return position;
+		if (position >= size)
+			return position;
 	}
 	return size;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 bool BufferedFile::Open(const std::wstring& path, unsigned flags)
 {
 	if (BinaryFile::Open(path, flags))
@@ -100,15 +100,16 @@ bool BufferedFile::Open(const std::wstring& path, unsigned flags)
 		m_FilePos = 0;
 		m_SeekError = false;
 
-		m_pBuffer = static_cast<uint8_t*>(malloc(m_BufferSize));
-		if (m_pBuffer) return true;
+		m_Buffer = static_cast<uint8_t*>(malloc(m_BufferSize));
+		if (m_Buffer) return true;
 
 		BinaryFile::Close();
 	}
+
 	return false;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 inline void BufferedFile::PurgeCache()
 {
 	size_t bytesLeft = m_CachedSize - m_BufferPos;
@@ -118,35 +119,40 @@ inline void BufferedFile::PurgeCache()
 	m_SeekError |= (bytesLeft != 0);
 }
 
-//----------------------------------------------------------------------------------------------------------------------
-bool BufferedFile::Read(void* pBuffer, size_t bytesToRead, size_t& bytesRead)
+//--------------------------------------------------------------------------------------------------------------------------------
+bool BufferedFile::Read(void* buffer, size_t bytesToRead, size_t& bytesRead)
 {
 	bytesRead = 0;
 
-	if (!m_pBuffer) return false;
-	if (!bytesToRead) return true;
-	if (!pBuffer) return false;
+	if (!m_Buffer)
+		return false;
+	if (!bytesToRead)
+		return true;
+	if (!buffer)
+		return false;
 
 	if (!m_CachedSize && m_BufferPos && !WriteCache())
 		return false;
 
-	uint8_t* pOut = static_cast<uint8_t*>(pBuffer);
+	uint8_t* out = static_cast<uint8_t*>(buffer);
 	for (bool secondPass = false;; secondPass = true)
 	{
 		size_t bytesLeft = m_CachedSize - m_BufferPos;
 		size_t toRead = (bytesToRead <= bytesLeft) ? bytesToRead : bytesLeft;
 
-		const uint8_t* pIn = &m_pBuffer[m_BufferPos];
+		const uint8_t* in = &m_Buffer[m_BufferPos];
 #if !AML_DEBUG
 		if (toRead < 16)
 		{
 			for (size_t i = 0; i < toRead; ++i)
-				pOut[i] = pIn[i];
+				out[i] = in[i];
 		} else
-			memcpy(pOut, pIn, toRead);
+		{
+			memcpy(out, in, toRead);
+		}
 #else
 		if (toRead)
-			memcpy(pOut, pIn, toRead);
+			memcpy(out, in, toRead);
 #endif
 		m_BufferPos += toRead;
 		bytesRead += toRead;
@@ -155,29 +161,30 @@ bool BufferedFile::Read(void* pBuffer, size_t bytesToRead, size_t& bytesRead)
 		if (!bytesToRead || secondPass)
 			return true;
 
-		pOut += toRead;
+		out += toRead;
 		m_BufferPos = m_CachedSize = 0;
 
 		size_t bytesActuallyRead = 0;
 		if (bytesToRead >= m_BufferSize)
 		{
-			bool res = ReadFromFile(pOut, bytesToRead, bytesActuallyRead);
-			if (res) bytesRead += bytesActuallyRead;
+			bool res = ReadFromFile(out, bytesToRead, bytesActuallyRead);
+			if (res)
+				bytesRead += bytesActuallyRead;
 			return res;
 		}
-		if (!ReadFromFile(m_pBuffer, m_BufferSize, bytesActuallyRead))
+		if (!ReadFromFile(m_Buffer, m_BufferSize, bytesActuallyRead))
 			return false;
 		m_CachedSize = bytesActuallyRead;
 	}
 }
 
-//----------------------------------------------------------------------------------------------------------------------
-AML_NOINLINE bool BufferedFile::ReadFromFile(void* pBuffer, size_t bytesToRead, size_t& bytesRead)
+//--------------------------------------------------------------------------------------------------------------------------------
+AML_NOINLINE bool BufferedFile::ReadFromFile(void* buffer, size_t bytesToRead, size_t& bytesRead)
 {
 	if (!m_SeekError || BinaryFile::SetPosition(m_FilePos))
 	{
 		m_SeekError = true;
-		if (auto rr = BinaryFile::Read(pBuffer, bytesToRead); rr.second)
+		if (auto rr = BinaryFile::Read(buffer, bytesToRead); rr.second)
 		{
 			bytesRead = rr.first;
 			m_FilePos += rr.first;
@@ -188,18 +195,20 @@ AML_NOINLINE bool BufferedFile::ReadFromFile(void* pBuffer, size_t bytesToRead, 
 	return false;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 bool BufferedFile::SetPosition(long long position)
 {
-	if (!m_pBuffer || (position < 0)) return false;
+	if (!m_Buffer || (position < 0))
+		return false;
 	if (m_BufferPos && !m_CachedSize && !WriteCache())
 		return false;
 
 	// Если в буфере есть прочитанные данные и новая позиция остается в пределах буфера,
-	// то мы можем изменить только m_BufferPos, не меняя реальную текущую позицию файла.
+	// то мы можем изменить только m_BufferPos, не меняя реальную текущую позицию файла
 	if (m_CachedSize && (position < m_FilePos) && (position >= static_cast<long long>(m_FilePos - m_CachedSize)))
+	{
 		m_BufferPos = static_cast<size_t>(position - m_FilePos + m_CachedSize);
-	else
+	} else
 	{
 		m_BufferPos = 0;
 		m_CachedSize = 0;
@@ -213,74 +222,89 @@ bool BufferedFile::SetPosition(long long position)
 	return true;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 bool BufferedFile::Truncate()
 {
-	if (!m_pBuffer) return false;
+	if (!m_Buffer)
+		return false;
 
 	if (m_CachedSize)
+	{
 		PurgeCache();
+	}
 	else if (m_BufferPos && !WriteCache())
+	{
 		return false;
+	}
 
 	if (m_SeekError)
 	{
 		m_SeekError = !BinaryFile::SetPosition(m_FilePos);
-		if (m_SeekError) return false;
+		if (m_SeekError)
+			return false;
 	}
 
 	return BinaryFile::Truncate();
 }
 
-//----------------------------------------------------------------------------------------------------------------------
-bool BufferedFile::Write(const void* pBuffer, size_t bytesToWrite)
+//--------------------------------------------------------------------------------------------------------------------------------
+bool BufferedFile::Write(const void* buffer, size_t bytesToWrite)
 {
-	if (!m_pBuffer) return false;
-	if (!bytesToWrite) return true;
-	if (!pBuffer) return false;
+	if (!m_Buffer)
+		return false;
+	if (!bytesToWrite)
+		return true;
+	if (!buffer)
+		return false;
 
-	if (m_CachedSize) PurgeCache();
+	if (m_CachedSize)
+		PurgeCache();
+
 	while (bytesToWrite >= m_BufferSize - m_BufferPos)
 	{
 		if (m_BufferPos)
 		{
-			if (!WriteCache()) return false;
+			if (!WriteCache())
+				return false;
 			if (bytesToWrite < m_BufferSize)
 				break;
 		}
-		return WriteToFile(pBuffer, bytesToWrite);
+		return WriteToFile(buffer, bytesToWrite);
 	}
 
 #if !AML_DEBUG
 	if (bytesToWrite < 16)
 	{
-		uint8_t* pOut = &m_pBuffer[m_BufferPos];
-		const uint8_t* pIn = static_cast<const uint8_t*>(pBuffer);
+		uint8_t* out = &m_Buffer[m_BufferPos];
+		const uint8_t* in = static_cast<const uint8_t*>(buffer);
 		for (size_t i = 0; i < bytesToWrite; ++i)
-			pOut[i] = pIn[i];
-	} else
+			out[i] = in[i];
+	} else 
 #endif
-	memcpy(&m_pBuffer[m_BufferPos], pBuffer, bytesToWrite);
+	{
+		memcpy(&m_Buffer[m_BufferPos], buffer, bytesToWrite);
+	}
 
 	m_BufferPos += bytesToWrite;
 	return true;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 bool BufferedFile::WriteCache()
 {
-	bool res = WriteToFile(m_pBuffer, m_BufferPos);
-	if (res) m_BufferPos = 0;
+	bool res = WriteToFile(m_Buffer, m_BufferPos);
+	if (res)
+		m_BufferPos = 0;
 	return res;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
-AML_NOINLINE bool BufferedFile::WriteToFile(const void* pData, size_t bytesToWrite)
+//--------------------------------------------------------------------------------------------------------------------------------
+AML_NOINLINE bool BufferedFile::WriteToFile(const void* data, size_t bytesToWrite)
 {
 	if (!m_SeekError || BinaryFile::SetPosition(m_FilePos))
 	{
 		m_SeekError = true;
-		if (BinaryFile::Write(pData, bytesToWrite))
+		if (BinaryFile::Write(data, bytesToWrite))
 		{
 			m_FilePos += bytesToWrite;
 			m_SeekError = false;
--- source/mdpn/aml/core/file.h
+++ source/mdpn/aml/core/file.h
@@ -12,59 +12,60 @@
 
 namespace util {
 
-//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------
 class BufferedFile : public BinaryFile
 {
 public:
 	// Параметр bufferSize задает размер буфера в килобайтах. Этот буфер будет использован
-	// для операций чтения (упреждающее чтение) или операций записи (кеширование записи).
+	// для операций чтения (упреждающее чтение) или операций записи (кеширование записи)
 	BufferedFile(size_t bufferSize = 16);
 	virtual ~BufferedFile();
 
-	virtual bool		Open(const std::wstring& path, unsigned flags = FILE_OPEN_READ);
+	virtual bool Open(const std::wstring& path, unsigned flags = FILE_OPEN_READ);
+
 	// Закрывает файл. Если во внутреннем буфере есть несохраненные данные, то будет вызвана функция
 	// FlushBuffer(). Однако при этом не гарантируется, что данные будут полностью сохранены в файл.
 	// При возниковении ошибки записи в файл данные могут быть сохранены частично или могут быть не
 	// сохранены вовсе. Чтобы гарантированно сохранить накопленные в буфере данные в файл, нужно
-	// перед вызовом Close() вызвать функцию FlushBuffer() и проверить возвращенное ей значение.
-	virtual void		Close();
+	// перед вызовом Close() вызвать функцию FlushBuffer() и проверить возвращенное ей значение
+	virtual void Close();
 
-	using				BinaryFile::Read;
-	// Читает не более bytesToRead байт из файла в pBuffer. Если во внутреннем буфере
+	using BinaryFile::Read;
+	// Читает не более bytesToRead байт из файла в buffer. Если во внутреннем буфере
 	// есть несохраненные данные, то они будут сохранены в файл до операциии чтения.
-	// При этом, если во время записи произойдет ошибка, функция вернет false.
-	virtual bool		Read(void* pBuffer, size_t bytesToRead, size_t& bytesRead);
-	// Записывает bytesToWrite байт из pBuffer в файл. По возможности (при достаточности
+	// При этом, если во время записи произойдет ошибка, функция вернет false
+	virtual bool Read(void* buffer, size_t bytesToRead, size_t& bytesRead);
+	// Записывает bytesToWrite байт из buffer в файл. По возможности (при достаточности
 	// внутреннего буфера) данные будут кешированы. Чтобы сохранить кешированные данные
-	// в файл, нужно использовать функцию FlushBuffer() или функцию Flush().
-	virtual bool		Write(const void* pBuffer, size_t bytesToWrite);
+	// в файл, нужно использовать функцию FlushBuffer() или функцию Flush()
+	virtual bool Write(const void* buffer, size_t bytesToWrite);
 
 	// Сохраняет накопленное содержимое внутреннего буфера (кеш записи)
-	// в файл, а затем сбрасывает системный буфер файла на накопитель.
-	virtual bool		Flush();
+	// в файл, а затем сбрасывает системный буфер файла на накопитель
+	virtual bool Flush();
 	// Сохраняет накопленное содержимое внутреннего буфера записи в файл (при
-	// этом запись данных в файл может кешироваться операционной системой).
-	bool				FlushBuffer();
+	// этом запись данных в файл может кешироваться операционной системой)
+	bool FlushBuffer();
 
-	virtual long long	GetSize() const;
-	virtual long long	GetPosition() const;
-	virtual bool		SetPosition(long long position);
-	virtual bool		Truncate();
-
-protected:
-	void				PurgeCache();
-	bool				WriteCache();
-
-	bool				ReadFromFile(void* pBuffer, size_t bytesToRead, size_t& bytesRead);
-	bool				WriteToFile(const void* pData, size_t bytesToWrite);
+	virtual long long GetSize() const;
+	virtual long long GetPosition() const;
+	virtual bool SetPosition(long long position);
+	virtual bool Truncate();
 
 protected:
-	uint8_t*			m_pBuffer;		// Указатель на внутренний буфер
-	const size_t		m_BufferSize;	// Размер внутреннего буфера в байтах
-	size_t				m_BufferPos;	// Позиция чтения/записи внутри буфера
-	size_t				m_CachedSize;	// Байт данных в буфере (режим чтения)
-	mutable long long	m_FilePos;		// Позиция файла, соответствующая состоянию буфера
-	mutable bool		m_SeekError;	// false, если m_FilePos равен реальной позиции файла
+	void PurgeCache();
+	bool WriteCache();
+
+	bool ReadFromFile(void* buffer, size_t bytesToRead, size_t& bytesRead);
+	bool WriteToFile(const void* data, size_t bytesToWrite);
+
+protected:
+	uint8_t* m_Buffer;				// Указатель на внутренний буфер
+	const size_t m_BufferSize;		// Размер внутреннего буфера в байтах
+	size_t m_BufferPos;				// Позиция чтения/записи внутри буфера
+	size_t m_CachedSize;			// Байт данных в буфере (режим чтения)
+	mutable long long m_FilePos;	// Позиция файла, соответствующая состоянию буфера
+	mutable bool m_SeekError;		// false, если m_FilePos равен реальной позиции файла
 };
 
 } // namespace util
